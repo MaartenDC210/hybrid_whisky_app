5498d082b68cec2661d17ac635335b7a33ce6e62
mdc <de-marre@hotmail.com>
2023-11-06 20:59:01 +0100

    fix build

--- src/Firebase/initialize.jsx
+++ src/Firebase/initialize.jsx
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getMessaging, getToken } from "firebase/messaging";
 import { initializeFirestore, persistentLocalCache } from "firebase/firestore";
 import { firebaseConfig } from "./config";
 import { getAuth } from "firebase/auth";
@@ -8,7 +7,4 @@ export const firebaseApp = initializeApp(firebaseConfig);
 
 export const firestoreDB = initializeFirestore(firebaseApp, {localCache: persistentLocalCache(/*settings*/{})});
 export const firebaseAuth = getAuth(firebaseApp);
-export const messaging = getMessaging(firebaseApp);
-export const messagingToken = getToken( messaging, {vapidKey: firebaseConfig.vapidKey});
-
 
--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,30 +1,14 @@
 import { Box } from '@mui/material';
 import { getAuth } from "firebase/auth";
-import { getMessaging, onMessage } from "firebase/messaging";
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 
 export const Home = () => {
   const auth = getAuth();
   const user = auth.currentUser;
-  const message = "";
-
-  const messaging = getMessaging();
-  onMessage(messaging, (payload) => {
-    console.log('Message received. ', payload);
-    // ...
-  });
   
   return (
-    
-    <Box>
-      {
       user ? 
       <Box>{user.email}</Box> :
       <Box>Home</Box>
-      }
-      <Box>
-        {{ message }}
-      </Box>
-    </Box>
   );
 };
